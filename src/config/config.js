3a51c91507c30155b1a18554e77c00b135e7cebf
jhonatan Padilla <jhoalparo1991@gmail.com>
2023-01-12 14:08:27 -0500

    Se hizo la configuracion  inicial del servidor

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const Config = {
+    port : process.env.PORT || 3000,
+    enviroment : process.env.NODE_ENV || 'development'
+}
+
+
+module.exports = {
+    Config
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
+require('dotenv').config()
+const express = require('express');
+const { Config } = require('./config/config');
+// Initialization
+const app = express();
+
+
+// Settings
+
+
+
+// Run server
+app.listen(Config.port,()=> { 
+    console.log(`Server on pott ${Config.port}`);
+})
+
+
+
 
 
