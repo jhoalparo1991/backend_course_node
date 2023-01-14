e9f070368f299834a7870f8594bc764a271a61b9
Jhonatan Padilla <jhoalparo1991@gmail.com>
2023-01-13 22:03:36 -0500

    se configuro el archivo docker para mysql y variables de entorno

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,21 @@
 const Config = {
     port: process.env.PORT || 3000,
     enviroment: process.env.NODE_ENV || 'development',
-    dbUser: process.env.DB_USER,
-    dbPassword: process.env.DB_PASSWORD,
-    dbPort: process.env.DB_PORT,
-    dbName: process.env.DB_NAME,
-    dbHostname: process.env.DB_HOSTNAME,
+    
+    dbPostgresUser: process.env.DB_POSTGRES_USER,
+    dbPostgresPassword: process.env.DB_POSTGRES_PASSWORD,
+    dbPostgresPort: process.env.DB_POSTGRES_PORT,
+    dbPostgresName: process.env.DB_POSTGRES_NAME,
+    dbPostgresHostname: process.env.DB_POSTGRES_HOSTNAME,
+
+    dbMysqlUser: process.env.DB_MYSQL_USER,
+    dbMysqlPassword: process.env.DB_MYSQL_PASSWORD,
+    dbMysqlPort: process.env.DB_MYSQL_PORT,
+    dbMysqlName: process.env.DB_MYSQL_NAME,
+    dbMysqlHostname: process.env.DB_MYSQL_HOSTNAME,
+
+    dialect: process.env.DB_DIALECT
+
 }
 
 
