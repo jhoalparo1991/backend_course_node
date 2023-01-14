4791ae62c14048b17335291464240d77a8ec8a7a
Jhonatan Padilla <jhoalparo1991@gmail.com>
2023-01-13 22:23:49 -0500

    se modifico los datos de la conexion

--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -5,10 +5,11 @@ const setupModels = require('../db/models/');
 const dbUser = encodeURIComponent(Config.dbUser);  
 const dbPassword = encodeURIComponent(Config.dbPassword);  
 
-const URL = `postgres://${dbUser}:${dbPassword}@${Config.dbHostname}:${Config.dbPort}/${Config.dbName}`;
+
+const URL = `${Config.dialect}://${dbUser}:${dbPassword}@${Config.dbHostname}:${Config.dbPort}/${Config.dbName}`;
 
 const sequelize = new Sequelize(URL,{
-    dialect:'postgres',
+    dialect:Config.dialect,
     logging: console.log
 })
 
